01bfd01942085120378d72fb6d2600344d1e346a
julien <julienl6900669006@gmail.com>
2020-05-29 09:58:43 +0200

    update of template (unuse)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,10 +1,38 @@
 <?php
 
-
 namespace App\DataFixtures;
 
+use App\Entity\Student;
+use App\Entity\Score;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
-class AppFixtures
+class AppFixtures extends Fixture
 {
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
 
+        for ($p = 0; $p < 50; $p++) {
+            $student = new Student;
+            $student->setLastname($faker->lastName)
+                ->setFirstname($faker->firstName)
+                ->setBirthday($faker->dateTimeBetween($startDate = '-30 years', $endDate = 'now', $timezone = null));
+
+            $manager->persist($student);
+
+            for ($c = 0; $c < mt_rand(3, 5); $c++) {
+                $score = new Score;
+                $score->setSubject($faker->jobTitle)
+                    ->setScore(rand(0,20))
+                    ->setStudent($student);
+
+                $manager->persist($score);
+            }
+        }
+
+        $manager->flush();
+    }
 }
+
